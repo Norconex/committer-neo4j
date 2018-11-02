0d6f3ce6d53b5b0c72dfa80048f6d7ff4f710c2e
sroussy <srylvain.roussy@free.fr>
2018-11-02 10:23:57 +0100

    Initial commit

---
+++ norconex-committer-neo4j/src/main/java/norconex/committer/neo4j/Neo4jCommitter.java
@@ -0,0 +1,33 @@
+package norconex.committer.neo4j;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.commons.configuration.XMLConfiguration;
+
+import com.norconex.committer.core.AbstractMappedCommitter;
+import com.norconex.committer.core.ICommitOperation;
+
+public class Neo4jCommitter extends AbstractMappedCommitter{
+
+	@Override
+	protected void saveToXML(XMLStreamWriter writer) throws XMLStreamException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void loadFromXml(XMLConfiguration xml) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void commitBatch(List<ICommitOperation> batch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
