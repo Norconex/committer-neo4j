e689f1f3160e04be2f243dc174494bfc6a1c362a
Sylvain ROUSSY <sylvain.roussy@free.fr>
2019-10-09 10:22:24 +0200

    Add regexFilter attribute on relationship element to filter pages    Add label attribute on sourcePropertyKey element and targetPropertyKey element to filter nodes with labels fix: Default values are not injected #2 fix: targetFindSyntax attribute not documented #3

---
+++ norconex-committer-neo4j/src/main/java/com/norconex/committer/neo4j/NodeProperty.java
@@ -0,0 +1,21 @@
+package com.norconex.committer.neo4j;
+
+public class NodeProperty {
+
+	private String propertyKey;
+	private String label;
+		
+	public String getPropertyKey() {
+		return propertyKey;
+	}
+	public void setPropertyKey(String propertyKey) {
+		this.propertyKey = propertyKey;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+}
